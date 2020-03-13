4a1e30853930fe80206e3a6ea0c4817126e57a4f
Boris Resnick <boris@resnick.ru>
2020-03-13 14:49:42 +0300

    Before changing HTTP server

--- android/ScoutFence/app/src/main/java/ru/glonassunion/aerospace/scoutfence/LWIntegrationServer.java
+++ android/ScoutFence/app/src/main/java/ru/glonassunion/aerospace/scoutfence/LWIntegrationServer.java
@@ -13,12 +13,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import fi.iki.elonen.NanoHTTPD;
+import org.json.JSONObject;
 
 public class LWIntegrationServer extends NanoHTTPD {
     private ILorawanJsonHandler handler = null;
-
+   
     public LWIntegrationServer(int port, ILorawanJsonHandler handler) {
         super(port);
         this.handler = handler;
@@ -26,22 +29,36 @@ public class LWIntegrationServer extends NanoHTTPD {
 
     @Override
     public Response serve(IHTTPSession session) {
+        Log.i("LoRaWAN", 
+            "Processing incoming HTTP request with JSON payload from " + session.getUri() +
+            " (method " + session.getMethod() + ")");
         Map<String, String> files = new HashMap<String, String>();
         try {
             session.parseBody(files);
         } catch (IOException ioe) {
+            Log.e("LoRaWAN", "IOException while receiving JSON " + ioe.getMessage());
             return newFixedLengthResponse(Response.Status.INTERNAL_ERROR, MIME_PLAINTEXT, "SERVER INTERNAL ERROR: IOException: " + ioe.getMessage());
         } catch (ResponseException re) {
+            Log.e("LoRaWAN", "ResponseException while receiving JSON " + re.getMessage());
             return newFixedLengthResponse(re.getStatus(), MIME_PLAINTEXT, re.getMessage());
         }
+
+        // NB: Nasty nanoHTTPD handles different headers differently
         final String body = session.getQueryParameterString();
+        final String postData = files.get("postData");
 
         Handler mainThreadHangler = new Handler(Looper.getMainLooper());
 
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                handler.HandleLorawanJSON(body);
+                if(body != null)
+                {
+                    handler.HandleLorawanJSON(body);
+                } else {
+                    handler.HandleLorawanJSON(postData);
+                }
+
             }
         };
         mainThreadHangler.post(runnable);
